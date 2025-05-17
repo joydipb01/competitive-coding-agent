22e33f300c167dad03b19f70c70e4c4f65e203f6
Joydip Kishore Bhattacharyya <joydipb01@gmail.com>
2025-05-17 17:23:12 +0530

    :tada: Added agent folder and included LLM Manager

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# competitive-coding-agent
+# LangGraph Agent for Competitive Coding
 Implementation of a LangGraph agent powered by a fine-tuned LLM to solve competitive coding problems
---
+++ agent/llm_manager.py
@@ -0,0 +1,45 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, GenerationConfig, pipeline, AutoTokenizer
+import torch
+
+class LLMManager:
+
+    MODEL_NAME = "anonymous4chan/llama-2-7b"
+
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME,
+        torch_dtype=torch.float16,
+        device_map="auto",
+        trust_remote_code=True,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_NAME,
+        trust_remote_code=True,
+        use_fast=True
+    )
+    generation_config = GenerationConfig.from_pretrained(
+        MODEL_NAME,
+        trust_remote_code=True,
+    )
+    generation_config.temperature = 0.0001
+    generation_config.top_p = 0.95
+    generation_config.top_k = 50
+    generation_config.max_new_tokens = 1024
+
+    code-pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        generation_config=generation_config,
+    )
+
+    def __init__(self):
+        self.llm = HuggingFacePipeline(
+            pipeline=self.code-pipeline
+        )
+    
+    def invoke(self, prompt: PromptTemplate, **kwargs  ) -> str:
+        messages = prompt.format_messages(**kwargs)
+        response = self.llm.invoke(messages)
+        return response.content
